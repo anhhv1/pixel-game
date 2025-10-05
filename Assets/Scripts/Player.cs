633ebfe488a69a1b4619820833989af86475b2c0
unknown <vietanh123456123@gmail.com>
2025-10-05 19:40:55 +0700

    update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
     private Rigidbody2D rb;
     private Animator anim;
 
@@ -13,11 +12,20 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
 
+    public bool canDoubleJump;
+
     [Header("Collision info")]
     private bool isGrounded;
+    private bool isAirborne;
+    private float xInput;
+    private float coyoteTimeCounter;
+    private float jumpBufferCounter;
+    private bool isWallDetected;
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private LayerMask groundLayer;
-    private float xInput;
+    [SerializeField] private float coyoteTime = 0.2f;
+    [SerializeField] private float jumpBufferTime = 0.2f;
+    [SerializeField] private float wallCheckDistance;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -25,40 +33,86 @@ public class Player : MonoBehaviour
     }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateAirbornStatus();
         HandleCollisions();
         HandleInput();
         HandleAnimations(xInput);
         HandleMovement();
     }
 
+    private void UpdateAirbornStatus()
+    {
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+            if (isAirborne)
+            {
+                Handlelanding();
+            }
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+            if (!isAirborne)
+            {
+                isAirborne = true;
+            }
+        }
+    }
+
+    private void Handlelanding()
+    {
+        canDoubleJump = true;
+        isAirborne = false;
+    }
+
     private void HandleInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
 
-
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Jump();
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if (jumpBufferCounter > 0)
+        {
+            DoubleJump();
+            jumpBufferCounter = 0;
         }
     }
-
-    private void Jump()
+    private void DoubleJump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (isGrounded || coyoteTimeCounter > 0)
+        {
+            Jump();
+            coyoteTimeCounter = 0;
+        }
+        else if (canDoubleJump)
+        {
+            Jump();
+            canDoubleJump = false;
+        }
     }
+    private void Jump() => rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 
     private void HandleCollisions()
     {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
+        isWallDetected = Physics2D.Raycast(transform.position, new Vector2(transform.localScale.x, 0), wallCheckDistance, groundLayer);
     }
 
-    private void HandleAnimations( float xInput)
+    private void HandleAnimations(float xInput)
     {
         if (xInput != 0)
         {
@@ -73,7 +127,7 @@ public class Player : MonoBehaviour
         }
         anim.SetFloat("xVelocity", rb.velocity.x);
         anim.SetFloat("yVelocity", rb.velocity.y);
-        anim.SetBool("isGrounded", !isGrounded);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private void HandleMovement()
@@ -84,6 +138,6 @@ public class Player : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x,transform.position.y - groundCheckDistance));
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
